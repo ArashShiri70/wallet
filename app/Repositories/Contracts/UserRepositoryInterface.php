8deb11fdf06fe0fa55dd47d8bca1bd5b511b4e63
ashiri <a.shiri@netafraz.com>
2024-12-30 13:49:48 +0330

    fix: refactor function name and consider relations default param for functions which required

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -4,9 +4,9 @@ namespace App\Repositories\Contracts;
 
 interface UserRepositoryInterface
 {
-    public function getAllUsers();
-    public function getSpecificColumns(array $columns);
-    public function findUserById($id);
+    public function getAllUsers($relations = []);
+    public function getUsersWithSpecificColumns(array $columns, $relations = []);
+    public function findUserById($id, $relations = []);
     public function createUser(array $data);
     public function updateUser($id, array $data);
     public function deleteUser($id);
